dc84a487c385220192dc8dc143abb5b9a8387c4a
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2021-08-27 01:21:04 +0200

    initial commit

---
+++ WeatherClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 27.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherClone/WeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneApp.swift
+//  WeatherClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 27.08.21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
