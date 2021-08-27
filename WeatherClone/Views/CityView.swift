ecd1eda84ccbd06f71574b063f7733f788c8685e
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2021-08-27 14:38:26 +0200

    added new view, already detached from static text

---
+++ WeatherClone/Views/CityView.swift
@@ -0,0 +1,36 @@
+//
+//  CityView.swift
+//  WeatherClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 27.08.21.
+//
+
+import SwiftUI
+
+struct CityView: View {
+    var city: String
+    var weatherDescription: String
+    var tempNow: Int
+    var maxTemp: Int
+    var minTemp: Int
+    
+    var body: some View {
+        VStack {
+            Text(city)
+                .font(.largeTitle)
+            Text(weatherDescription)
+                .font(.title3)
+            Text("\(tempNow)º")
+                .font(.system(size: 90))
+            Text("H:\(maxTemp)º L:\(minTemp)º")
+                .font(.title3)
+        }
+    }
+}
+
+struct CityView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityView(city: "Berlin", weatherDescription: "Cloudy", tempNow: 13, maxTemp: 17, minTemp: 11)
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
